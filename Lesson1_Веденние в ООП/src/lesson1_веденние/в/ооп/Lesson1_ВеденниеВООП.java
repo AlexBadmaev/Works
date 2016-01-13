fa0a45431ba10b296c1a640f7acbb1afe2b5cafc
Alexander <ibaragi7@gmail.com>
2016-01-13 19:07:48 +0200

    fierst commit

---
+++ Lesson1_Веденние в ООП/src/lesson1_веденние/в/ооп/Lesson1_ВеденниеВООП.java
@@ -0,0 +1,27 @@
+
+package lesson1_веденние.в.ооп;
+public class Lesson1_ВеденниеВООП {
+
+   public static void main(String[] args) {
+       int a;
+       a = 12;
+       int b;
+       b = 13;
+       int c = 14;
+       int d = 15;
+       
+   if(b < 13)
+   {
+    
+   System.out.println(a);
+   }
+   if(a < 12){
+   System.out.println(b);
+   }
+
+  if(c == 14){
+      System.out.println(c);
+    }
+    
+   }
+}
